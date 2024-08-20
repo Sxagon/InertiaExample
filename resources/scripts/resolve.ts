6bace3b6f74a2b778f3e6b8ca02b19f33d23122b
Michal Lipka <82515195+Sxagon@users.noreply.github.com>
2024-08-20 18:47:51 +0200

    Update resolve.ts
    live updating on vue/ts/js file change fix

--- resources/scripts/resolve.ts
+++ resources/scripts/resolve.ts
@@ -1,13 +1,15 @@
-const allPages = import.meta.glob("../views/pages/**/*.vue")
+const allPages = (import.meta as any).globEager("../views/**/*.vue")
 
 export default function (name: string, pages: Record<string, any> = allPages) {
+    const pathToFind = `${name.replaceAll(".", "/")}.vue`;
+
     for (const path in pages) {
-        if (path.endsWith(`${name.replaceAll(".", "/")}.vue`)) {
+        if (path.endsWith(pathToFind)) {
             return typeof pages[path] === "function"
                 ? pages[path]()
-                : pages[path]
+                : pages[path];
         }
     }
 
-    throw new Error(`Page not found: ${name}`)
+    throw new Error(`Page not found: ${name}`);
 }
